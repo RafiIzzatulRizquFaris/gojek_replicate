626e0ccd42318f5d639713de4ae2b3fae88a80cc
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-05-01 12:56:37 +0700

    update appbar

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,42 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends AppBar{
+class CustomAppBar extends AppBar {
+  CustomAppBar()
+      : super(
+            elevation: 0.25,
+            backgroundColor: Colors.white,
+            flexibleSpace: gojekAppBar());
 
-  CustomAppBar(): super(elevation: 0.25, backgroundColor: Colors.white, flexibleSpace: gojekAppBar());
-
-  static Widget gojekAppBar(){
+  static Widget gojekAppBar() {
     return Container(
-      padding: EdgeInsets.only(left: 16, right: 16),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 3),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Image.asset("assets/images/img_gojek_logo.png", height: 50, width: 100,),
-          Row(
-            children: [
-              Container(
-                height: 28,
-                width: 28,
-                padding: EdgeInsets.all(6),
+          Expanded(
+            flex: 7,
+            child: Container(
+//              padding: EdgeInsets.only(top: 2),
+              child: Container(
+                padding: EdgeInsets.all(8),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
-                    color: Colors.orangeAccent
-                ),
-                alignment: Alignment.centerRight,
-                child: Icon(
-                  Icons.local_bar,
-                  color: Colors.white,
-                  size: 16,
+                    border: Border(
+                        top: BorderSide(color: Colors.grey),
+                        right: BorderSide(color: Colors.grey),
+                        bottom: BorderSide(color: Colors.grey),
+                        left: BorderSide(color: Colors.grey))),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                    Text(
+                      "Cari layanan Gojek...",
+                      style: TextStyle(fontSize: 14),
+                    )
+                  ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0x50FFD180)
-                ),
-                child: Text("2.441 points", style: TextStyle(fontSize: 14),),
-              )
-            ],
+//              child: Image.asset("assets/images/img_gojek_logo.png", height: 50, width: 100,),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Color(0x50FFD180)),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      height: 28,
+                      width: 28,
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.orangeAccent,
+                      ),
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.cake,
+                        color: Colors.white,
+                        size: 16,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        "Promo",
+                        style: TextStyle(fontSize: 14),
+                        overflow: TextOverflow.clip,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
